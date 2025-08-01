b3ef2937a8d01647a93e2a6e2ed1f9fdaf7329ef
Lucas Santos <lucas201273@gmail.com>
2025-07-31 22:02:26 -0300

    chore: add spring project

---
+++ src/test/java/br/com/chronotrack/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.chronotrack.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
