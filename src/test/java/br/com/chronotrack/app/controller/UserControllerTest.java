7cf5cc812929f0ec176a94120c108d2220079a87
Lucas Santos <lucas201273@gmail.com>
2025-08-03 12:11:16 -0300

    test: ensure UserController returns 400 if no password is provided

--- src/test/java/br/com/chronotrack/app/controller/UserControllerTest.java
+++ src/test/java/br/com/chronotrack/app/controller/UserControllerTest.java
@@ -5,6 +5,7 @@ import br.com.chronotrack.app.domain.dto.request.UserRequestDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -53,6 +54,18 @@ class UserControllerTest {
   @Test
   void shouldReturnsBadRequestIfAnInvalidEmailIsProvided() throws Exception{
     UserRequestDto dto = new UserRequestDto("any_name", "any_mail", "any_password");
+    ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.post("/signup")
+      .content(parseObjectToString(dto))
+      .contentType(MediaType.APPLICATION_JSON)
+      .accept(MediaType.APPLICATION_JSON)
+    );
+    resultActions.andExpect(MockMvcResultMatchers.status().isBadRequest());
+  }
+
+  @DisplayName("should returns 400 if no password is provided")
+  @Test
+  void shouldReturnsBadRequestIfNoPasswordIsProvided() throws Exception{
+    UserRequestDto dto = new UserRequestDto("any_name", "any_mail@mail.com", null);
     ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.post("/signup")
       .content(parseObjectToString(dto))
       .contentType(MediaType.APPLICATION_JSON)
