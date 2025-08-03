6dfcdb12f2eb5cc7596ec2fdede62e26a1987007
Lucas Santos <lucas201273@gmail.com>
2025-08-03 11:41:39 -0300

    test: ensure UserController returns 400 if no email is provided

--- src/test/java/br/com/chronotrack/app/controller/UserControllerTest.java
+++ src/test/java/br/com/chronotrack/app/controller/UserControllerTest.java
@@ -2,7 +2,6 @@ package br.com.chronotrack.app.controller;
 
 
 import br.com.chronotrack.app.domain.dto.request.UserRequestDto;
-import br.com.chronotrack.app.domain.entities.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,10 +10,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.web.client.match.MockRestRequestMatchers;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.assertj.MockMvcTester;
+
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
@@ -28,7 +26,7 @@ class UserControllerTest {
   @Autowired
   private ObjectMapper objectMapper;
 
-  @DisplayName("Should returns 400 if no name is provided")
+  @DisplayName("should returns 400 if no name is provided")
   @Test
   void shouldReturnsBadRequestIfNoNameIsProvided() throws Exception{
     UserRequestDto dto = new UserRequestDto( null,"any_mail@mail.com", "any_password");
@@ -37,6 +35,19 @@ class UserControllerTest {
       .content(jsonBody)
       .contentType(MediaType.APPLICATION_JSON)
       .accept(MediaType.APPLICATION_JSON));
+    resultActions.andExpect(MockMvcResultMatchers.status().isBadRequest());
+  }
+
+  @DisplayName("should returns 400 if no email is provided")
+  @Test
+  void shouldReturnsBadRequestIfNoEmailIsProvided() throws Exception{
+    UserRequestDto dto = new UserRequestDto("any_name", null, "any_password");
+    String jsonBody = objectMapper.writeValueAsString(dto);
+    ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.post("/signup")
+      .content(jsonBody)
+      .contentType(MediaType.APPLICATION_JSON)
+      .accept(MediaType.APPLICATION_JSON)
+    );
     resultActions.andExpect(MockMvcResultMatchers.status().isBadRequest());
   }
 
